fc2a60a9c0c7756934dd79128933e94c4cbd6594
mycroft <3243106232@qq.com>
2023-05-30 20:49:16 +0800

    write menu

---
+++ game/static/js/src/playground/zbase.js
@@ -0,0 +1,22 @@
+class AcGamePlayground {
+    constructor(root) {
+        this.root = root;
+        this.$playground = $(`<div>游戏界面</div>`);
+
+        this.hide();
+        this.root.$ac_game.append(this.$playground);
+
+        this.start;
+    }
+
+    start() {
+    }
+
+    show() { //打开playground
+        this.$playground.show();
+    }
+
+    hide() { // 关闭playground
+        this.$playground.hide();
+    }
+}
